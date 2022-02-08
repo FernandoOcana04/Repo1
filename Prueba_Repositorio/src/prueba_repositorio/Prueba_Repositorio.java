ffceaf4f69b5db2796ef3bc671a6a83e6ec01ffe
FernandoOcana <97054566+FernandoOcana@users.noreply.github.com>
2022-02-08 10:07:09 -0600

    Se ha implementado la suma en la calculadora

--- Prueba_Repositorio/src/prueba_repositorio/Prueba_Repositorio.java
+++ Prueba_Repositorio/src/prueba_repositorio/Prueba_Repositorio.java
@@ -4,6 +4,8 @@
  */
 package prueba_repositorio;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Fernando Rubén Ocaña
@@ -14,7 +16,16 @@ public class Prueba_Repositorio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        int num1, num2;
+        int suma, resta, multiplicacion, division;
+        
+        System.out.println("Ingrese dos números: ");
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+        
+        suma = num1 + num2;
+        System.out.println("La suma es: " + suma);
     }
     
 }
